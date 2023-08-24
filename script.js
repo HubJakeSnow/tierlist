c8e07af77acb3a6c8cdf94330d1f4b7f6e68a331
Jake Snow <jakesnow@gmx.at>
2023-08-24 14:59:08 +0200

    Add row/column button functionality

--- script.js
+++ script.js
@@ -2,11 +2,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const rowList = document.getElementById("rowList");
     const addRowButton = document.querySelector(".add-row");
+    const columnContainer = document.getElementById("columnContainer");
+    const addColumnButton = document.querySelector(".add-column");
 
-    let currentRowNumber = 4; // Start with the next row number
+    let currentRowNumber = 4; // Starts with the next row number
+    let addedColumnCount = 0; // Tracks the number of columns added
 
     addRowButton.addEventListener("click", function (event) {
-        event.preventDefault(); // Prevent form submission
+        event.preventDefault(); // Prevent form submission and webpage reloading
         
         const newRow = document.createElement("li");
         currentRowNumber++; // Increment row number
@@ -17,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const input = document.createElement("input");
         input.setAttribute("type", "text");
         input.setAttribute("name", `row-${currentRowNumber}`);
-        input.setAttribute("placeholder", String.fromCharCode(65 + currentRowNumber - 2));
+        input.setAttribute("placeholder", String.fromCharCode(65 + currentRowNumber - 2)); // Character calculation
 
         const button = document.createElement("button");
         button.classList.add("remove");
@@ -29,6 +32,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
         rowList.appendChild(newRow);
     });
-});
 
-// Adds a column input box 
+    // Adds a column input box
+    addColumnButton.addEventListener("click", function (event) {
+        event.preventDefault(); // Prevent form submission
+        
+        addedColumnCount++;
+
+    // Adds two input boxes on the first click, then one for next clicks
+    const numInputBoxes = (addedColumnCount === 1) ? 2 : 1;
+
+    for (let i = 0; i < numInputBoxes; i++) {
+        const newInput = document.createElement("input");
+        newInput.setAttribute("type", "text");
+        newInput.setAttribute("name", `column-${addedColumnCount}`);
+        columnContainer.appendChild(newInput);
+    }
+    });
+});
